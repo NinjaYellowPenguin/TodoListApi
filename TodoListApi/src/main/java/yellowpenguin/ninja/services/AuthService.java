eb259724fa6dbc52518fc6083248b33ac9b9246c
Carlos Espinosa Diz <carlos.unity.developer@gmail.com>
2025-05-11 13:43:38 +0200

    refactor: change TokenRepo for TokenService in AuthService

--- TodoListApi/src/main/java/yellowpenguin/ninja/services/AuthService.java
+++ TodoListApi/src/main/java/yellowpenguin/ninja/services/AuthService.java
@@ -1,7 +1,5 @@
 package yellowpenguin.ninja.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,105 +9,57 @@ import jakarta.transaction.Transactional;
 import yellowpenguin.ninja.dto.LoginUserRequest;
 import yellowpenguin.ninja.dto.RegisterUserRequest;
 import yellowpenguin.ninja.dto.TokenResponse;
-import yellowpenguin.ninja.models.Token;
-import yellowpenguin.ninja.models.Token.TokenType;
 import yellowpenguin.ninja.models.User;
-import yellowpenguin.ninja.repos.TokenRepository;
 
 @Service
 public class AuthService {
 	@Autowired
 	private UserService userService;
 	@Autowired
-	private TokenRepository tokenRepo;
-	@Autowired
 	private JwtService jwtService;
 	@Autowired
 	private AuthenticationManager authManager;
 
 	@Transactional
-	public TokenResponse register(RegisterUserRequest request) {
-		
+	public TokenResponse register(RegisterUserRequest request) {		
 		User savedUser = userService.createUser(request);
-		
-		String token = jwtService.generateToken(savedUser);
-		String refreshToken = jwtService.generateRefreshToken(savedUser);
-		saveToken(savedUser, token);
-		
-		TokenResponse tokenResponse = new TokenResponse();
-		tokenResponse.setAccessToken(token);
-		tokenResponse.setRefreshToken(refreshToken);
-		return tokenResponse;
-	}
-
-	private void saveToken(User savedUser, String tokenString) {
-		Token token = new Token();
-		token.setUser(savedUser);
-		token.setToken(tokenString);
-		token.setTokenType(TokenType.BEARER);
-		token.setExpired(false);
-		token.setRevoked(false);
-		tokenRepo.save(token);
-	}
+		TokenResponse response = jwtService.createTokens(savedUser);		
+		return response;
+	}	
 
 	public TokenResponse login(LoginUserRequest request) {
 		authManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
 		User user  = userService.findByEmail(request.getEmail());
-		String token = jwtService.generateToken(user);
-		String refreshToken = jwtService.generateRefreshToken(user);
-		TokenResponse response = new TokenResponse();
-		response.setAccessToken(token);
-		response.setRefreshToken(refreshToken);
-		revokeAllUsersTokens(user);
+		TokenResponse response = jwtService.createTokens(user);
+		jwtService.revokeAllUsersTokens(user);
 		return response;
-	}
-
-	private void revokeAllUsersTokens(User user) {
-		List<Token> validUserTokens = tokenRepo.findAllValidIsFalseOrRevokedIsFalseByUserId(user.getId());
-		if(!validUserTokens.isEmpty()) {
-			for(Token token: validUserTokens) {
-				token.setExpired(true);
-				token.setRevoked(true);
-			}
-			tokenRepo.saveAll(validUserTokens);
-		}		
-	}
+	}	
 
 	public TokenResponse refreshToken(String authHeader) {
-		if(authHeader == null || !authHeader.startsWith("Bearer ")){
-			throw new IllegalArgumentException("Invalid bearer token");
-		}
-		final String refreshToken = authHeader.substring(7);
+		final String refreshToken = extractRefreshToken(authHeader);		
+		final String email = extractEmail(refreshToken);
+		final User user = userService.findByEmail(email);		
+		return jwtService.refreshToken(refreshToken, user);
+	}
+	
+	private String extractEmail(String refreshToken) {
 		final String userEmail = jwtService.extractUsername(refreshToken);
 		if(userEmail == null) {
 			throw new IllegalArgumentException("Invalid Email Refresh Token");
 		}
-		final User user = userService.findByEmail(userEmail);
-		
-		if(!jwtService.isTokenValid(refreshToken, user)) {
-			throw new IllegalArgumentException("Invalid Refresh Token");
-		}
-		final String accessToken = jwtService.generateToken(user);
-		revokeAllUsersTokens(user);
-		saveToken(user, accessToken);
-		
-		TokenResponse response = new TokenResponse();
-		response.setAccessToken(accessToken);
-		response.setRefreshToken(refreshToken);
-		return response;
+		return userEmail;
 	}
-	
-	public void logout(String token) {
-		
-		if(token == null || !token.startsWith("Bearer ")) {
-			throw new IllegalArgumentException("Invalid Token");		}
-		
-		final String jwtToken = token.substring(7);		
-		final Token foundToken = tokenRepo.findByToken(jwtToken).orElseThrow(()->new IllegalArgumentException("Invalid Token Logout."));		
-		foundToken.setExpired(true);
-		foundToken.setRevoked(true);
-		tokenRepo.save(foundToken);
-		
+
+	private String extractRefreshToken(String authHeader) {
+		if(authHeader == null || !authHeader.startsWith("Bearer ")){
+			throw new IllegalArgumentException("Invalid bearer token");
+		}
+		return authHeader.substring(7);
+	}
+
+	public void logout(String token) {		
+		final String jwtToken = extractRefreshToken(token);	
+		jwtService.revokeToken(jwtToken);		
 	}
 
 
--- TodoListApi/src/main/java/yellowpenguin/ninja/services/JwtService.java
+++ TodoListApi/src/main/java/yellowpenguin/ninja/services/JwtService.java
@@ -1,10 +1,12 @@
 package yellowpenguin.ninja.services;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,12 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.transaction.Transactional;
+import yellowpenguin.ninja.dto.TokenResponse;
+import yellowpenguin.ninja.models.Token;
 import yellowpenguin.ninja.models.User;
+import yellowpenguin.ninja.repos.TokenRepository;
+import yellowpenguin.ninja.models.Token.TokenType;
 
 @Service
 public class JwtService {
@@ -24,10 +31,34 @@ public class JwtService {
 	@Value("${jwt.refresh-token-expiration-time}")
 	private long jwtRefresh;
 	
-	public String generateToken(User savedUser) {
+	@Autowired
+	private TokenRepository repo;
+	
+	public TokenResponse createTokens(User user) {
+		String token = generateToken(user);
+		String refreshToken = generateRefreshToken(user);
+		saveToken(user, token);
+		
+		TokenResponse tokenResponse = new TokenResponse();
+		tokenResponse.setAccessToken(token);
+		tokenResponse.setRefreshToken(refreshToken);
+		return tokenResponse;
+	}
+	
+	private void saveToken(User user, String tokenString) {
+		Token token = new Token();
+		token.setUser(user);
+		token.setToken(tokenString);
+		token.setTokenType(TokenType.BEARER);
+		token.setExpired(false);
+		token.setRevoked(false);
+		repo.save(token);
+	}
+	
+	private String generateToken(User savedUser) {
 		return buildToken(savedUser, jwtExpiration);
 	}
-	public String generateRefreshToken(User savedUser) {
+	private String generateRefreshToken(User savedUser) {
 		return buildToken(savedUser, jwtRefresh);
 	}
 
@@ -39,8 +70,39 @@ public class JwtService {
 		.issuedAt(new Date(System.currentTimeMillis()))
 		.expiration(new Date(System.currentTimeMillis() + expiration))
 		.signWith(getSignInKey())
-		.compact();
-		
+		.compact();		
+	}
+	
+	@Transactional
+	public TokenResponse refreshToken(String refreshToken, User user) {
+		if(!isTokenValid(refreshToken, user)) {
+			throw new IllegalArgumentException("Invalid User Refresh Token");
+		}
+		final String accessToken = generateToken(user);
+		revokeAllUsersTokens(user);
+		saveToken(user, accessToken);		
+		TokenResponse response = new TokenResponse();
+		response.setAccessToken(accessToken);
+		response.setRefreshToken(refreshToken);
+		return response;
+	}
+	
+	public void revokeToken(String token) {
+		Token foundToken = repo.findByToken(token).orElseThrow(()->new IllegalArgumentException("Invalid Token Logout."));		
+		foundToken.setExpired(true);
+		foundToken.setRevoked(true);
+		repo.save(foundToken);		
+	}
+	
+	public void revokeAllUsersTokens(User user) {
+		List<Token> validUserTokens = repo.findAllValidIsFalseOrRevokedIsFalseByUserId(user.getId());
+		if(!validUserTokens.isEmpty()) {
+			for(Token token: validUserTokens) {
+				token.setExpired(true);
+				token.setRevoked(true);
+			}
+			repo.saveAll(validUserTokens);
+		}		
 	}
 	
 	private SecretKey getSignInKey() {
@@ -67,4 +129,6 @@ public class JwtService {
 				.getPayload();
 		return jwtToken.getExpiration().before(new Date());
 	}
+
+	
 }
