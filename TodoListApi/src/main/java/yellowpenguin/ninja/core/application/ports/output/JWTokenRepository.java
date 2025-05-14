8b56088d3677846a872e1cdf7156d8c38995e441
Carlos Espinosa Diz <carlos.unity.developer@gmail.com>
2025-05-14 11:42:11 +0200

    refactor: add user+token repo

--- TodoListApi/src/main/java/yellowpenguin/ninja/core/application/ports/output/JWTokenRepository.java
+++ TodoListApi/src/main/java/yellowpenguin/ninja/core/application/ports/output/JWTokenRepository.java
@@ -1,5 +1,13 @@
 package yellowpenguin.ninja.core.application.ports.output;
 
-public class JWTokenRepository {
+import java.util.List;
+
+import yellowpenguin.ninja.core.domain.entities.JWToken;
+
+public interface JWTokenRepository {
+	
+	public JWToken save(JWToken jwToken);
+	public JWToken findByToken(String token);
+	public List<JWToken> findAllValidIsFalseOrRevokedIsFalseByUserId(String userId);
 
 }
